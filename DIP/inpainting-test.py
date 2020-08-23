12c205ef2c340772ef45125a64eba3282db5c201
Yun-Chun Chen <yunchunchen@Yun-Chuns-MacBook-Pro.local>
2020-08-24 01:45:13 +0800

    code released

--- DIP/inpainting-test.py
+++ DIP/inpainting-test.py
@@ -5,9 +5,6 @@ import matplotlib.pyplot as plt
 
 import os
 import cv2
-import ipdb
-import random
-import pickle
 import argparse
 import numpy as np
 from skimage.measure import compare_psnr
@@ -37,11 +34,8 @@ def parse_args():
     parser.add_argument('--noise_method', dest='noise_method',default='noise', type=str)
     parser.add_argument('--input_depth', dest='input_depth', default=32, type=int)
     parser.add_argument('--output_path', dest='output_path',default='results/inpainting', type=str)
-    parser.add_argument('--batch_size', dest='batch_size',default=1, type=int)
-    parser.add_argument('--net', dest='net',default='default', type=str)
     parser.add_argument('--reg_noise_std', dest='reg_noise_std', default=0.03, type=float)
-    parser.add_argument('--i_NAS', dest='i_NAS', default=-1, type=int)
-    parser.add_argument('--save_png', dest='save_png', default=0, type=int)
+    parser.add_argument('--image_name', type=str)
 
     args = parser.parse_args()
     return args
@@ -51,128 +45,97 @@ if __name__ == '__main__':
 
     args = parse_args()
 
-    global_path = args.output_path
+    # Choose figure
+    img_path = 'data/inpainting/' + args.image_name
 
-    if not os.path.exists(global_path):
-        os.makedirs(global_path)
+    # Load image
+    img_pil, img_np = get_image(img_path, -1)
+    img_np          = nn.ReflectionPad2d(1)(np_to_torch(img_np))[0].numpy()
+    img_pil         = np_to_pil(img_np)
 
-    PSNR_mat = np.empty((0, args.num_iter), dtype=np.float32)
+    img_mask    = get_bernoulli_mask(img_pil, 0.50)
+    img_mask_np = pil_to_np(img_mask)
 
-    img_path_list = []
-    psnr_gt_best_list = []
+    img_masked  = img_np * img_mask_np
+    mask_var    = np_to_torch(img_mask_np).type(dtype)
 
-    for image_name in img_path_list:
+    # Visualization
+    if args.plot:
+        plot_image_grid([img_np, img_mask_np, img_mask_np * img_np], 3, 11);
 
-        if args.save_png == 1 and not os.path.exists(os.path.join(global_path, image_name)):
-            os.makedirs(os.path.join(global_path, image_name))
+    from models.model_inpainting import Model
+    net = Model()
 
-        # Choose figure
-        img_path = 'data/inpainting/' + image_name + '_GT.png'
+    net = net.type(dtype)
 
-        # Load image
-        img_pil, img_np = get_image(img_path, -1)
-        img_np          = nn.ReflectionPad2d(1)(np_to_torch(img_np))[0].numpy()
-        img_pil         = np_to_pil(img_np)
+    net_input = get_noise(args.input_depth, args.noise_method, img_np.shape[1:]).type(dtype).detach()
 
-        img_mask    = get_bernoulli_mask(img_pil, 0.50)
-        img_mask_np = pil_to_np(img_mask)
+    mse = torch.nn.MSELoss().type(dtype)
 
-        img_masked  = img_np * img_mask_np
-        mask_var    = np_to_torch(img_mask_np).type(dtype)
+    img_var = np_to_torch(img_np).type(dtype)
 
-        # Visualization
-        if args.plot:
-            plot_image_grid([img_np, img_mask_np, img_mask_np * img_np], 3, 11);
+    net_input_saved = net_input.detach().clone()
+    noise           = net_input.detach().clone()
 
-        from models.model_inpainting import Model
-        net = Model()
+    last_net         = None
+    psrn_masked_last = 0
+    psnr_gt_best     = 0
 
-        net = net.type(dtype)
+    # Main
+    i  = 0
+    PSNR_list = []
 
-        net_input = get_noise(args.input_depth, args.noise_method, img_np.shape[1:]).type(dtype).detach()
+    _t = {'im_detect': Timer(), 'misc': Timer()}
 
-        mse = torch.nn.MSELoss().type(dtype)
+    def closure():
 
-        img_var = np_to_torch(img_np).type(dtype)
+        global i, psrn_masked_last, last_net, net_input, psnr_gt_best, PSNR_list
 
-        net_input_saved = net_input.detach().clone()
-        noise           = net_input.detach().clone()
+        _t['im_detect'].tic()
 
-        last_net         = None
-        psrn_masked_last = 0
-        psnr_gt_best     = 0
+        # Add variation
+        if args.reg_noise_std > 0:
+            net_input = net_input_saved + (noise.normal_() * args.reg_noise_std)
+        out = net(net_input)
 
-        # Main
-        i  = 0
-        PSNR_list = []
+        total_loss = mse(out * mask_var, img_var * mask_var)
+        total_loss.backward()
 
-        _t = {'im_detect' : Timer(), 'misc' : Timer()}
+        psrn_masked = compare_psnr(img_masked, out.detach().cpu().numpy()[0] * img_mask_np)
+        psrn        = compare_psnr(img_np,     out.detach().cpu().numpy()[0])
 
-        def closure():
+        PSNR_list.append(psrn)
 
-            global i, psrn_masked_last, last_net, net_input, psnr_gt_best, PSNR_list
+        if psrn > psnr_gt_best:
+            psnr_gt_best = psrn
 
-            _t['im_detect'].tic()
+        _t['im_detect'].toc()
 
-            # Add variation
-            if args.reg_noise_std > 0:
-                net_input = net_input_saved + (noise.normal_() * args.reg_noise_std)
-            #ipdb.set_trace()
-            out = net(net_input)
+        print ('Iteration %05d    Loss %f   PSNR_masked %f PSNR %f    Time %.3f'  % (i, total_loss.item(), psrn_masked, psrn, _t['im_detect'].total_time), '\r', end='')
 
-            total_loss = mse(out * mask_var, img_var * mask_var)
-            total_loss.backward()
+        # Backtracking
+        if args.plot and i % args.show_every == 0:
+            
+            plot_image_grid([np.clip(out_np, 0, 1)], factor=4, nrow=1)
 
-            psrn_masked = compare_psnr(img_masked, out.detach().cpu().numpy()[0] * img_mask_np)
-            psrn        = compare_psnr(img_np,     out.detach().cpu().numpy()[0])
+            out_np = torch_to_np(out)
 
-            PSNR_list.append(psrn)
+            if psrn_masked - psrn_masked_last < -5:
+                print('Falling back to previous checkpoint.')
 
-            if psrn > psnr_gt_best:
-                psnr_gt_best = psrn
+                for new_param, net_param in zip(last_net, net.parameters()):
+                    net_param.data.copy_(new_param.cuda())
 
-            _t['im_detect'].toc()
+                return total_loss*0
+            else:
+                last_net = [x.cpu() for x in net.parameters()]
+                psrn_masked_last = psrn_masked
 
-            print ('Iteration %05d    Loss %f   PSNR_masked %f PSNR %f    Time %.3f'  % (i, total_loss.item(), psrn_masked, psrn, _t['im_detect'].total_time), '\r', end='')
+        i += 1
 
-            if i % args.show_every == 0:
-                if args.save_png == 1:
-                    out_np = torch_to_np(out)
-                    cv2.imwrite(os.path.join(global_path, image_name, str(i) + '.png'),\
-                    np.clip(out_np, 0, 1).transpose(1, 2, 0)[:,:,::-1] * 255)
+        return total_loss
 
-                if args.plot:
-                    plot_image_grid([np.clip(out_np, 0, 1)], factor=4, nrow=1)
-
-            # Backtracking
-            if args.plot and i % args.show_every == 0:
-                out_np = torch_to_np(out)
-
-                if psrn_masked - psrn_masked_last < -5:
-                    print('Falling back to previous checkpoint.')
-
-                    for new_param, net_param in zip(last_net, net.parameters()):
-                        net_param.data.copy_(new_param.cuda())
-
-                    return total_loss*0
-                else:
-                    last_net = [x.cpu() for x in net.parameters()]
-                    psrn_masked_last = psrn_masked
-
-            i += 1
-
-            return total_loss
-
-        p = get_params('net', net, net_input)
-        optimize(args.optimizer, p, closure, args.lr, args.num_iter)
-
-        PSNR_mat  = np.concatenate((PSNR_mat, np.array(PSNR_list).reshape(1,args.num_iter)), axis=0)
-        pickle.dump( PSNR_mat, open( os.path.join(global_path, 'PSNR.pkl'), "wb" ) )
-
-        psnr_gt_best_list.append(psnr_gt_best)
+    p = get_params('net', net, net_input)
+    optimize(args.optimizer, p, closure, args.lr, args.num_iter)
 
     print('Finish optimization\n')
-
-    for idx, image_name in enumerate(img_path_list):
-        print ('Image: %8s   PSNR: %.2f'  % (image_name, psnr_gt_best_list[idx]), '\n', end='')
-    print ('Averaged PSNR: %.2f'  % (np.mean(psnr_gt_best_list)), '\n', end='')
